c0ba66ca4f6a7973e91ab7865134d15d772464cc
DenisProkofiev <68288272+DenisProkofiev@users.noreply.github.com>
2021-02-26 13:47:58 +0400

    second commit

--- src/main/java/SpiralMatrix.java
+++ src/main/java/SpiralMatrix.java
@@ -3,37 +3,30 @@ import helpers.PrintHelper;
 import java.util.Arrays;
 
 public class SpiralMatrix {
-        public static int side = 7;
-        public static int count = 0;
-        public static int[][] arr = new int[side][side];
-
-        public static void fillSpiralMatrix() {
-            if (arr.length <= 0) {
-                throw new IllegalArgumentException();
-            }
+        public static void fillSpiralMatrix(int side) {
+           int[][] arr = new int[side][side];
+           int count = 0;
 
             for (int x = 0; x < arr.length / 2; x++) {
-                fillSquare(arr, x);
+                for (int i = x; i < arr.length - x; i++) {
+                    arr[x][i] = ++count;
+                }
+                for (int i = x + 1; i < arr.length - x; i++) {
+                    arr[i][arr.length - x - 1] = ++count;
+                }
+                for (int i = arr.length - 2 - x; i >= x; i--) {
+                    arr[arr.length - x - 1][i] = ++count;
+                }
+                for (int i = arr.length - 2 - x; i > x; i--) {
+                    arr[i][x] = ++count;
+                }
             }
+
             if (arr.length % 2 != 0) {
                 arr[arr.length / 2][arr.length / 2] = ++count;
             }
-            PrintHelper.printMatrix(arr);
-        }
 
-        private static void fillSquare(int[][] arr, int x) {
-            for (int i = x; i < arr.length - x; i++) {
-                arr[x][i] = ++count;
-            }
-            for (int i = x + 1; i < arr.length - x; i++) {
-                arr[i][arr.length - x - 1] = ++count;
-            }
-            for (int i = arr.length - 2 - x; i >= x; i--) {
-                arr[arr.length - x - 1][i] = ++count;
-            }
-            for (int i = arr.length - 2 - x; i > x; i--) {
-                arr[i][x] = ++count;
-            }
+            PrintHelper.printMatrix(arr);
         }
 
 }
---
+++ src/test/java/Test.java
@@ -0,0 +1,8 @@
+import helpers.PrintHelper;
+
+public class Test {
+
+    public static void main(String[] args) {
+        SpiralMatrix.fillSpiralMatrix(5);
+    }
+}
